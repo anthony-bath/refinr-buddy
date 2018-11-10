1c9cf6198b8144378ba972e4b9fc257a7e09351d
Anthony Bath <anthony@crushingit.io>
2018-11-09 22:35:23 -0500

    cloud functions  - added first iteration of create refinement function

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,27 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+admin.initializeApp();
+
+exports.createRefinementSession = functions.https.onRequest((req, res) => {
+  const id = Math.random()
+    .toString()
+    .slice(2, 9);
+
+  const { tickets, durationMinutes } = req.body;
+
+  admin
+    .database()
+    .ref(`refinement/${id}`)
+    .set({
+      tickets,
+      durationMinutes,
+      startDate: Date.now(),
+    })
+    .then(_ => {
+      return res.status(201).send();
+    })
+    .catch(err => {
+      return res.status(400).send(err);
+    });
+});
