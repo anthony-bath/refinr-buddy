bfc184fa5f309cf140bb678620675893ef5867bb
Anthony Bath <anthony.bath@live.com>
2018-11-13 22:30:32 -0500

    viewing a refinement session - fixed up firebase imports - added first firebase listener in the View component - stubbed out some rendering based on status of the session - added a Status enum

---
+++ src/config/enumeration.js
@@ -0,0 +1,5 @@
+export const Status = {
+  NotStarted: 0,
+  InProgress: 1,
+  Completed: 2,
+};
--- src/pages/view/View.js
+++ src/pages/view/View.js
@@ -1,12 +1,62 @@
 import React, { Component } from 'react';
+import firebase from 'firebase/app';
+import 'firebase/database';
+
+import { Status } from '../../config/enumeration';
 
 export default class View extends Component {
+  state = {
+    loading: true,
+    session: null,
+  };
+
+  componentDidMount() {
+    const { id } = this.props.match.params;
+
+    firebase
+      .database()
+      .ref(`refinement/${id}`)
+      .on('value', this.refinementUpdated);
+  }
+
+  componentWillUnmount() {
+    const { id } = this.props.match.params;
+
+    firebase
+      .database()
+      .ref(`refinement/${id}`)
+      .off('value', this.refinementUpdated);
+  }
+
+  refinementUpdated = snapshot => {
+    const session = snapshot.val();
+
+    this.setState({
+      session,
+      loading: false,
+    });
+  };
+
   render() {
-    return (
-      <div>
-        <h1>View Refinement</h1>
-        <button onClick={() => this.props.history.push('/')}>Back</button>
-      </div>
-    );
+    const { loading, session } = this.state;
+
+    if (loading) {
+      return <div>Loading...</div>;
+    } else if (!session) {
+      return (
+        <div>Refinement Session Id {this.props.match.params.id} not found.</div>
+      );
+    }
+
+    switch (session.status) {
+      case Status.NotStarted:
+        return <div>Refinment not yet started!</div>;
+      case Status.Completed:
+        return <div>The Refinement Session has been completed.</div>;
+      case Status.InProgress:
+        return <div>Refinement in progress</div>;
+      default:
+        return <div>Invalid Refinement Session</div>;
+    }
   }
 }
