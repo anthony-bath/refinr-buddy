fb296b617b3f0d70bcb37cfc9012b128cd2ecc4b
Anthony Bath <anthony.bath@live.com>
2018-11-19 21:24:51 -0500

    cloud functions update - added the endRefinementSession function

--- functions/index.js
+++ functions/index.js
@@ -2,9 +2,11 @@ const admin = require('firebase-admin');
 const { createRefinementSession } = require('./createRefinementSession');
 const { startRefinementSession } = require('./startRefinementSession');
 const { nextTicket } = require('./nextTicket');
+const { endRefinementSession } = require('./endRefinementSession');
 
 admin.initializeApp();
 
 exports.createRefinementSession = createRefinementSession;
 exports.startRefinementSession = startRefinementSession;
 exports.nextTicket = nextTicket;
+exports.endRefinementSession = endRefinementSession;
